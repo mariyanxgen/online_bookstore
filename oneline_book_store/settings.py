b777ac32a7a4191cc24c1144ec9a5bb86672fe8b
Your Name <your.email@example.com>
2025-11-28 21:07:01 +0530

    Update settings and add requirements.txt

--- oneline_book_store/settings.py
+++ oneline_book_store/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8f0md&%0sc59&05@_=@*_8$zh^99l5jli!3t=^+qjbjt1_@)#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
